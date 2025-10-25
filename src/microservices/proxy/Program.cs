69d02bb17b1295ae81767defa1f8a2a8b58a15c0
masny.pavel <pvmasny@gmail.com>
2025-10-25 14:11:28 +0300

    proxy impl

--- src/microservices/proxy/Program.cs
+++ src/microservices/proxy/Program.cs
@@ -1,15 +1,27 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using proxy;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Configuration.AddJsonFile("appsettings.json").AddEnvironmentVariables();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddOcelot();
+
+var moviesMigrationPercent = builder.Configuration.GetSection("MOVIES_MIGRATION_PERCENT").Get<int>();
+var gradualMigration = builder.Configuration.GetSection("GRADUAL_MIGRATION").Get<bool?>() ?? true;
+
+builder.Services.AddOcelot().AddCustomLoadBalancer<WeightedRoundRobinBalancer>(
+            (provider, route, discoveryProvider) =>
+                new WeightedRoundRobinBalancer(
+                    discoveryProvider.GetAsync,
+                    new int[] { 100-moviesMigrationPercent, moviesMigrationPercent },
+                    gradualMigration
+                )
+        );
 
 
 var app = builder.Build();
@@ -28,15 +40,11 @@ app.UseHttpsRedirection();
 app.MapControllers();
 app.MapGet("/health", () =>
  new { status = "success" });
-//await app.UseOcelot();
 
 
 app.UseWhen(context => !context.Request.Path.StartsWithSegments("/health"), appBuilder =>
 {
     appBuilder.UseOcelot().Wait();
 });
-
-
-
 
 app.Run();
